79791b6b47d6a1b01b706e750395e3ac9b810057
jvaz11 <juanvme123@gmail.com>
2015-10-16 03:02:18 -0700

    Fixed conditional to depend solely on pathname

--- 13/app.js
+++ 13/app.js
@@ -1,4 +1,4 @@
-/**sss
+/**
 * 13 -  HTTP JSON API Server
 Write an HTTP server that serves JSON data when it receives a GET request to the path '/api/parsetime'. Expect 
 the request to contain a query string with a key 'iso' and an ISO-format time as the value.
@@ -30,62 +30,42 @@ var http = require('http'),
 var port = process.argv[2];
 
 var server = http.createServer(function (req, res) {
-    // Check the method property of the request to ensure the request is a POST request
+    // Check the method property of the request to ensure the request is a GET request
     if (req.method === 'GET') {
 
         // Get the req.url object
         var parsedurl = url.parse(req.url, true);
         var pathName = parsedurl.pathname;
-        // /api/parsetime
         
         // Get the query string
         var queryStringObj = parsedurl.query;
-        
-        // Determine the format by getting the name of the first property of the query string object 
-        var format = Object.keys(queryStringObj)[0]; // iso
-        console.log('format', format);
 
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        
+        // Get the query parameter
+        var queryStringObj = parsedurl.query;
 
         if (pathName === '/api/parsetime') {
-
-
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-        
-            // Get the query parameter
-            var unixTimeObject = parsedurl.query;
-            console.log(unixTimeObject);
-
-            // var queryStringObj = parsedurl.query;
-            // {"iso":"2013-08-10T12:10:15.474Z"}
-            // var isoTime = new Date(queryStringObj.iso);
-            // console.log(isoTime.getMinutes());
-            
-            ///////
-            
-            
-            if (format === 'iso') {
-                var queryStringObj = parsedurl.query;
-                var isoTime = new Date(queryStringObj.iso);
-                console.log('isotime', isoTime);
-                var isoTimeObject = {
-                    'hour': isoTime.getMonth(),
-                    "minute": isoTime.getMinutes(),
-                    "second": isoTime.getSeconds()
-                };
-                res.write(JSON.stringify(isoTimeObject));
-            } else if (format === 'unixtime') {
-                var queryStringObj = parsedurl.query;
-                var unixTimeObject = queryStringObj;
-                res.write(JSON.stringify(unixTimeObject));
-            } else {
-                console.log('Endpoint not found. Sorry.');
-            }
-            res.end();
+            var isoTime = new Date(queryStringObj.iso);
+            var isoTimeObject = {
+                'hour': isoTime.getHours(),
+                "minute": isoTime.getMinutes(),
+                "second": isoTime.getSeconds()
+            };
+            res.write(JSON.stringify(isoTimeObject));
+        } else if (pathName === '/api/unixtime') {
+            var unixTime = new Date(queryStringObj.iso).getTime();
+            res.write(JSON.stringify({ 'unixtime': unixTime }));
+        } else {
+            res.writeHead(404, 'Endpoint not found. Sorry.')
+            res.end()
         }
+        res.end();
+
 
     } else {
-        console.log('Request received, but it was not a POST request.');
-        res.end('Request received, but it was not a POST request.');
+        console.log('Request received, but it was not a GET request.');
+        res.end('Request received, but it was not a GET request.');
     }
 });
 
